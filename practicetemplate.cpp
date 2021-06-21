bf81378644616fc960daa24b51ea98f21807407e
fer2k2 <fer2k2@users.noreply.github.com>
2021-06-21 18:29:17 -0500

    Create practicetemplate.cpp

---
+++ practicetemplate.cpp
@@ -0,0 +1,39 @@
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <cctype>
+#include <climits>
+#include <cmath>
+#include <complex>
+#include <cstdarg>
+#include <cstdio>
+#include <deque>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <iterator>
+#include <list>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <random>
+#include <regex>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+#include <atomic>
+#include <thread>
+#include <mutex>
+#include <condition_variable>
+#include <future>
+
+int main(int argc, char *argv[])
+{
+    
+    return 0;
+}
